f94c69b833ee7be3b36306a7518761085610136c
Topidesta a.k.a EmangDasar <desta.08b@gmail.com>
2020-03-26 15:35:21 +0700

    add objek

--- src/App.js
+++ src/App.js
@@ -3,16 +3,36 @@ import "./Custom.css";
 import Judul from "./components/Judul";
 import Isi from "./components/Isi";
 
+const activities = [
+  {
+    timestamp: new Date().getTime(),
+    text: "Ate lunch",
+    user: {
+      id: 1,
+      name: "Nate",
+      avatar: "http://www.croop.cl/UI/twitter/images/doug.jpg"
+    },
+    comments: [{ from: "Ari", text: "Me too!" }]
+  },
+  {
+    timestamp: new Date().getTime(),
+    text: "Woke up early for a beautiful run",
+    user: {
+      id: 2,
+      name: "Ari",
+      avatar: "http://www.croop.cl/UI/twitter/images/doug.jpg"
+    },
+    comments: [{ from: "Nate", text: "I am so jealous" }]
+  }
+];
+
 function App() {
   return (
     <div className="demo">
       <div className="notificationsFrame">
         <div className="panel">
           <Judul title="Timeline" />
-          <Judul title="Profile" />
-          <Judul title="Settings" />
-          <Judul title="Chat" />
-          <Isi />
+          <Isi activities={activities} />
         </div>
       </div>
     </div>
