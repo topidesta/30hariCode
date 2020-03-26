04cf33642fa2e31728e1767375a57448a64de1db
Topidesta a.k.a EmangDasar <desta.08b@gmail.com>
2020-03-26 15:56:45 +0700

    Passing data to props in multiple

--- src/App.js
+++ src/App.js
@@ -7,22 +7,22 @@ const activities = [
   {
     timestamp: new Date().getTime(),
     text: "Ate lunch",
-    user: {
+    pengguna: {
       id: 1,
-      name: "Nate",
+      nama: "Nate",
       avatar: "http://www.croop.cl/UI/twitter/images/doug.jpg"
     },
-    comments: [{ from: "Ari", text: "Me too!" }]
+    komentar: [{ from: "Ari", text: "Me too!" }]
   },
   {
     timestamp: new Date().getTime(),
     text: "Woke up early for a beautiful run",
-    user: {
+    pengguna: {
       id: 2,
-      name: "Ari",
+      nama: "Ari",
       avatar: "http://www.croop.cl/UI/twitter/images/doug.jpg"
     },
-    comments: [{ from: "Nate", text: "I am so jealous" }]
+    komentar: [{ from: "Nate", text: "I am so jealous" }]
   }
 ];
 
--- src/components/Isi.js
+++ src/components/Isi.js
@@ -2,22 +2,25 @@ import React, { Component } from "react";
 
 class Isi extends Component {
   render() {
+    const { activities } = this.props; // ES6 destructuring
+
     return (
       <div className="content">
         <div className="line"></div>
         {/* item timeline */}
-        <div className="item">
-          <div className="avatar">
-            <img
-              alt="Doug"
-              src="http://www.croop.cl/UI/twitter/images/doug.jpg"
-            />{" "}
-            Doug
-          </div>
-          <span className="time">An hour ago</span>
-          <p>Ate Lunch</p>
-          <div className="commentCount">2</div>
-        </div>
+        {activities.map(activity => {
+          return (
+            <div key={activity.pengguna.id} className="item">
+              <div className="avatar">
+                <img alt={activity.text} src={activity.pengguna.avatar} /> Doug
+              </div>
+              <span className="time">{activity.timestamp}</span>
+              <p>{activity.text}</p>
+              <div className="commentCount">{activity.komentar.length}</div>
+            </div>
+          );
+        })}
+
         {/* .... */}
       </div>
     );
