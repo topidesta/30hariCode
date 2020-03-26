ba7b08883efbda9236b3d64abd09627c8a3fd0bc
Topidesta a.k.a EmangDasar <desta.08b@gmail.com>
2020-03-26 11:09:35 +0700

    komponen judul

---
+++ src/components/Judul.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class Judul extends Component {
+  render() {
+    return (
+      <div className="panel">
+        <div className="header">
+          <div className="menuIcon">
+            <div className="dashTop"></div>
+            <div className="dashBottom"></div>
+            <div className="circle"></div>
+          </div>
+          <span className="title">TimeLine</span>
+          <input type="text" className="searchInput" placeholder="Search... " />
+          <div className="fa fa-search searchIcon"></div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Judul;
