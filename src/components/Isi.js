a8256d0e6e4f29613f1cf6babf28556ea1f87f1c
Desta Muchamad <desta.muchamad@my.gavilan.edu>
2020-03-28 23:33:43 +0700

    yeyyeyeyeye

--- src/components/Isi.js
+++ src/components/Isi.js
@@ -1,7 +1,15 @@
 import React, { Component } from "react";
 import ItemAktifitas from "./ItemAktifitas";
 
+// const data = require("./data.json"); // ambil data
+
 class Isi extends Component {
+  // constructor(props) {
+  //   this.state = {
+  //     activities: []
+  //   };
+  // }
+
   render() {
     const { activities } = this.props; // ES6 destructuring
     return (
